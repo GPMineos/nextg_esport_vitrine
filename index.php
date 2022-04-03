469079a5619f2cc5c4c990b2e17a00eef4b6999c
Pierre <pierre.gervais@socialmove.com>
2022-04-03 11:51:18 +0200

    - Add new asset for header and logo - WIP : index.php {add header, add news content, add right panel} - Update default style

--- index.php
+++ index.php
@@ -1,21 +1,102 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
+    </script>
+    <script src="js/navbar-behavior.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
     <link rel="stylesheet" href="styles/default.css">
     <title>NextG - Esport</title>
 </head>
+
 <body>
-    <header>
+    <!--
+    ###
+    ### Header that contain menu navigation
+    ###
+    -->
+    <header class="header">
+        <nav class="navbar navbar-expand-lg fixed-top">
+            <div class="container"><a href="#" style="padding: 0px;" class="navbar-brand text-uppercase font-weight-bold"><img height="64px" src="./assets/logo-1024x1024.png" /></a>
+                <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler navbar-toggler-right"><i class="fa fa-bars"></i></button>
 
+                <div id="navbarSupportedContent" class="collapse navbar-collapse">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active"><a href="index.php" class="nav-link text-uppercase font-weight-bold">Accueil</a></li>
+                        <li class="nav-item"><a href="event.php" class="nav-link text-uppercase font-weight-bold">Evenements</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     </header>
-    <section>
-
-    </section>
-    <footer>
-
-    </footer>
+    <!--
+    ###
+    ### Header text and all website content
+    ###
+    -->
+    <div class="container-fluid">
+        <div class="row pt-5 text-white">
+            <!--
+            ###
+            ### Header text
+            ###
+            -->
+            <header class="py-5 mt-5">
+                <div class="content">
+                    <h1 class="display-4">NextG - Esport</h1>
+                    <p class="lead mb-0">Le gaming fait maintenant parti de nos vies !</p>
+                    <p class="lead mb-0">
+                        <a href="#" class="text-white">
+                            <u>Inscrivez-vous à notre LAN</u></a>
+                    </p>
+                </div>
+            </header>
+            <!--
+            ###
+            ### Content that contain some new/actuality
+            ###
+            -->
+            <section class="col-lg-9 mt-5">
+                <p class="lead">Nous sommes heureux de vous annoncer l'ouverture de notre site internet et par la même occasion l'oganisation de notre premier <strong>tournoi</strong> en présentiel.<br>
+                    Nous esperons que vous allez être nombreux à participer à notre compétition qui se déroulera sur les jeux suivants :<br />
+                <ul>
+                    <li>CS:GO</li>
+                    <li>Rocket League</li>
+                    <li>League of legend</li>
+                </ul>
+                </p>
+            </section>
+            <!--
+            ###
+            ### Right pannel
+            ###
+            -->
+            <section class="col-lg-3 mt-5">
+                <h3>Nos evenements à venir</h3>
+                <!-- Card that represent incoming events-->
+                <div class="card text-black" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">NextG Lan - 18/09/2022</h5>
+                        <p class="card-text">NextG - Lan #1, venez participez à notre Lan</p>
+                        <ul>
+                            <li>Plus de 3500€ de cash prize !</li>
+                            <li>Plus de 700€ de lot à remporter</li>
+                            <li>Des stand avec differente activité, VR, Simulation</li>
+                        </ul>
+                        <a href="event.php" class="btn btn-primary">Visualiser</a>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
 </body>
+
 </html>
