4f79328c413cce0e15776ba8670637dfe98b9a5e
Pierre <pierre.gervais@socialmove.com>
2022-04-03 12:37:14 +0200

    - Add padding on site content

--- index.php
+++ index.php
@@ -6,13 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
-    </script>
-    <script src="js/navbar-behavior.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-    </script>
     <link rel="stylesheet" href="styles/default.css">
     <title>NextG - Esport</title>
 </head>
@@ -43,14 +37,14 @@
     ###
     -->
     <div class="container-fluid">
-        <div class="row pt-5 text-white">
+        <div class="row pt-4 text-white">
             <!--
             ###
             ### Header text
             ###
             -->
-            <header class="py-5 mt-5">
-                <div class="content">
+            <header class="header-text px-0 mt-5">
+                <div class="content py-5">
                     <h1 class="display-4">NextG - Esport</h1>
                     <p class="lead mb-0">Le gaming fait maintenant parti de nos vies !</p>
                     <p class="lead mb-0">
@@ -59,44 +53,91 @@
                     </p>
                 </div>
             </header>
+            <section class="row col-lg-12" style="padding-left: 12.5%; padding-right: 12.5%;">
             <!--
             ###
             ### Content that contain some new/actuality
             ###
             -->
-            <section class="col-lg-9 mt-5">
-                <p class="lead">Nous sommes heureux de vous annoncer l'ouverture de notre site internet et par la même occasion l'oganisation de notre premier <strong>tournoi</strong> en présentiel.<br>
-                    Nous esperons que vous allez être nombreux à participer à notre compétition qui se déroulera sur les jeux suivants :<br />
-                <ul>
-                    <li>CS:GO</li>
-                    <li>Rocket League</li>
-                    <li>League of legend</li>
-                </ul>
-                </p>
-            </section>
-            <!--
-            ###
-            ### Right pannel
-            ###
-            -->
-            <section class="col-lg-3 mt-5">
-                <h3>Nos evenements à venir</h3>
-                <!-- Card that represent incoming events-->
-                <div class="card text-black" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">NextG Lan - 18/09/2022</h5>
-                        <p class="card-text">NextG - Lan #1, venez participez à notre Lan</p>
-                        <ul>
-                            <li>Plus de 3500€ de cash prize !</li>
-                            <li>Plus de 700€ de lot à remporter</li>
-                            <li>Des stand avec differente activité, VR, Simulation</li>
-                        </ul>
-                        <a href="event.php" class="btn btn-primary">Visualiser</a>
+                <section class="col-lg-9 mt-5">
+                    <p class="lead">Nous sommes heureux de vous annoncer l'ouverture de notre site internet et par la même occasion l'oganisation de notre premier <strong>tournoi</strong> en présentiel.<br>
+                        Nous esperons que vous allez être nombreux à participer à notre compétition qui se déroulera sur les jeux suivants :<br />
+                    <ul>
+                        <li>CS:GO</li>
+                        <li>Rocket League</li>
+                        <li>League of legend</li>
+                    </ul>
+                    </p>
+                    <p class="lead">Nous sommes heureux de vous annoncer l'ouverture de notre site internet et par la même occasion l'oganisation de notre premier <strong>tournoi</strong> en présentiel.<br>
+                        Nous esperons que vous allez être nombreux à participer à notre compétition qui se déroulera sur les jeux suivants :<br />
+                    <ul>
+                        <li>CS:GO</li>
+                        <li>Rocket League</li>
+                        <li>League of legend</li>
+                    </ul>
+                    </p>
+                    <p class="lead">Nous sommes heureux de vous annoncer l'ouverture de notre site internet et par la même occasion l'oganisation de notre premier <strong>tournoi</strong> en présentiel.<br>
+                        Nous esperons que vous allez être nombreux à participer à notre compétition qui se déroulera sur les jeux suivants :<br />
+                    <ul>
+                        <li>CS:GO</li>
+                        <li>Rocket League</li>
+                        <li>League of legend</li>
+                    </ul>
+                    </p>
+                    <p class="lead">Nous sommes heureux de vous annoncer l'ouverture de notre site internet et par la même occasion l'oganisation de notre premier <strong>tournoi</strong> en présentiel.<br>
+                        Nous esperons que vous allez être nombreux à participer à notre compétition qui se déroulera sur les jeux suivants :<br />
+                    <ul>
+                        <li>CS:GO</li>
+                        <li>Rocket League</li>
+                        <li>League of legend</li>
+                    </ul>
+                    </p>
+                    <p class="lead">Nous sommes heureux de vous annoncer l'ouverture de notre site internet et par la même occasion l'oganisation de notre premier <strong>tournoi</strong> en présentiel.<br>
+                        Nous esperons que vous allez être nombreux à participer à notre compétition qui se déroulera sur les jeux suivants :<br />
+                    <ul>
+                        <li>CS:GO</li>
+                        <li>Rocket League</li>
+                        <li>League of legend</li>
+                    </ul>
+                    </p>
+                    <p class="lead">Nous sommes heureux de vous annoncer l'ouverture de notre site internet et par la même occasion l'oganisation de notre premier <strong>tournoi</strong> en présentiel.<br>
+                        Nous esperons que vous allez être nombreux à participer à notre compétition qui se déroulera sur les jeux suivants :<br />
+                    <ul>
+                        <li>CS:GO</li>
+                        <li>Rocket League</li>
+                        <li>League of legend</li>
+                    </ul>
+                    </p>
+                </section>
+                <!--
+                ###
+                ### Right pannel
+                ###
+                -->
+                <section class="col-lg-3 mt-5">
+                    <h4>Nos evenements à venir</h4>
+                    <!-- Card that represent incoming events-->
+                    <div class="card text-black" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">NextG Lan - 18/09/2022</h5>
+                            <p class="card-text">NextG - Lan #1, venez participez à notre Lan</p>
+                            <ul>
+                                <li>Plus de 3500€ de cash prize !</li>
+                                <li>Plus de 700€ de lot à remporter</li>
+                                <li>Des stand avec differente activité, VR, Simulation</li>
+                            </ul>
+                            <a href="event.php" class="btn btn-primary">Visualiser</a>
+                        </div>
                     </div>
-                </div>
+                </section>
             </section>
         </div>
     </div>
 </body>
-
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
+</script>
+<script src="js/navbar-behavior.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+</script>
 </html>
