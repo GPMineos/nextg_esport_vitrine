382db9bb3fd15ea7c2d583dd226637df745b8a9d
Pierre <pierre.gervais@socialmove.com>
2022-04-09 23:17:48 +0200

    - New index style, mobile first - Change some assets - Add comments - Fix navbar behavior js to checking scrollbar position onload - Update cdn

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<!--
+* ==========================================
+* Pierre Gervais
+* Studi évaluation
+* Site vitrine association
+* ==========================================
+*
+-->
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,8 +14,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="styles/default.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+    <script src="js/navbar-behavior.js" crossorigin="anonymous"></script>
     <title>NextG - Esport</title>
 </head>
 
@@ -20,38 +34,56 @@
     <header class="header">
         <nav class="navbar navbar-expand-lg fixed-top">
 
-            <a href="#" style="padding: 0px;" class="navbar-brand text-uppercase font-weight-bold"><img height="64px" src="./assets/logo-1024x1024.png" /></a>
-            <button type="button" data-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler collapsed navbar-toggler-right">
-                <svg xmlns="http://www.w3.org/2000/svg" color="white" width="32" height="32" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+            <a href="#" style="padding: 0px;" class="navbar-brand text-uppercase font-weight-bold"><img height="32px"
+                src="./assets/logo-1024x1024.png" /></a>
+            <div class="text-light"></div>
+            <button type="button" data-toggle="collapse" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation" class="navbar-toggler collapsed navbar-toggler-left">
+                <svg xmlns="http://www.w3.org/2000/svg" color="white" width="32" height="32" fill="currentColor"
+                    class="bi bi-list" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                        d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
                 </svg>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
                 <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active"><a href="index.php" class="nav-link text-uppercase font-weight-bold">Accueil</a></li>
-                    <li class="nav-item"><a href="event.php" class="nav-link text-uppercase font-weight-bold">Evenements</a></li>
+                    <li class="nav-item active"><a href="index.php"
+                            class="nav-link text-uppercase font-weight-bold">Accueil</a></li>
+                    <li class="nav-item"><a href="event.php"
+                            class="nav-link text-uppercase font-weight-bold">Evenements</a></li>
                 </ul>
 
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link" data-bs-html="true" href="#"><span style="padding-right: 20px;">Rejoignez nous sur les réseaux</span>
-                            <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                        <span class="nav-link">
+                            <span style="padding-right: 20px;">Rejoignez nous sur les réseaux</span>
+                        </span>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-bs-html="true" href="#">
+                            <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
                                 <title>Facebook</title>
-                                <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
+                                <path
+                                    d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
                             </svg>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#"><svg class="icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+                        <a class="nav-link" href="#"><svg class="icon" xmlns="http://www.w3.org/2000/svg" width="24"
+                                height="24" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
                                 <title>Twitter</title>
-                                <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
+                                <path
+                                    d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
                             </svg></a>
                     </li>
                 </ul>
             </div>
         </nav>
     </header>
+    !
     <!--
     ###
     ### Header text and all website content
@@ -64,92 +96,48 @@
             ### Header text
             ###
             -->
-            <header class="header-text px-0 mt-5">
+            <header class="header-text px-0" style="margin-top:10px;">
                 <div class="content py-5">
-                    <h1 class="display-4">NextG - Esport</h1>
-                    <p class="lead mb-0">Le gaming fait maintenant parti de nos vies !</p>
-                    <p class="lead mb-0">
-                        <a href="#" class="text-white">
-                            <u>Inscrivez-vous à notre LAN</u></a>
-                    </p>
+                    <h1 class="display-4">France Esport</h1>
+                    <p class="lead mb-0">France Esports, pour la promotion et le développement des Sports Électroniques</p>
                 </div>
             </header>
-            <section class="row col-lg-12" style="padding-left: 12.5%; padding-right: 12.5%;">
+            <section class="row" style="padding-left: 5%; padding-right: 5%;">
                 <!--
             ###
             ### Content that contain some new/actuality
             ###
             -->
-                <section class="col-md-9 mt-5">
-                    <p class="lead">Nous sommes heureux de vous annoncer l'ouverture de notre site internet et par la même occasion l'oganisation de notre premier <strong>tournoi</strong> en présentiel.<br>
-                        Nous esperons que vous allez être nombreux à participer à notre compétition qui se déroulera sur les jeux suivants :<br />
-                    <ul>
-                        <li>CS:GO</li>
-                        <li>Rocket League</li>
-                        <li>League of legend</li>
-                    </ul>
-                    </p>
-                    <p class="lead">Nous sommes heureux de vous annoncer l'ouverture de notre site internet et par la même occasion l'oganisation de notre premier <strong>tournoi</strong> en présentiel.<br>
-                        Nous esperons que vous allez être nombreux à participer à notre compétition qui se déroulera sur les jeux suivants :<br />
-                    <ul>
-                        <li>CS:GO</li>
-                        <li>Rocket League</li>
-                        <li>League of legend</li>
-                    </ul>
-                    </p>
-                    <p class="lead">Nous sommes heureux de vous annoncer l'ouverture de notre site internet et par la même occasion l'oganisation de notre premier <strong>tournoi</strong> en présentiel.<br>
-                        Nous esperons que vous allez être nombreux à participer à notre compétition qui se déroulera sur les jeux suivants :<br />
-                    <ul>
-                        <li>CS:GO</li>
-                        <li>Rocket League</li>
-                        <li>League of legend</li>
-                    </ul>
-                    </p>
-                    <p class="lead">Nous sommes heureux de vous annoncer l'ouverture de notre site internet et par la même occasion l'oganisation de notre premier <strong>tournoi</strong> en présentiel.<br>
-                        Nous esperons que vous allez être nombreux à participer à notre compétition qui se déroulera sur les jeux suivants :<br />
-                    <ul>
-                        <li>CS:GO</li>
-                        <li>Rocket League</li>
-                        <li>League of legend</li>
-                    </ul>
-                    </p>
-                    <p class="lead">Nous sommes heureux de vous annoncer l'ouverture de notre site internet et par la même occasion l'oganisation de notre premier <strong>tournoi</strong> en présentiel.<br>
-                        Nous esperons que vous allez être nombreux à participer à notre compétition qui se déroulera sur les jeux suivants :<br />
-                    <ul>
-                        <li>CS:GO</li>
-                        <li>Rocket League</li>
-                        <li>League of legend</li>
-                    </ul>
-                    </p>
+                <section class="col-lg-9 mt-5">
+                    <h4>Présentation de l’Association France Esports</h4>
+                    <p>Dans un contexte français, et international, de professionnalisation et de structuration de la 
+                        pratique des sports électroniques, les principaux acteurs de son développement en France 
+                        se sont associés avec les syndicats professionnels du jeu vidéo pour créer France Esports, 
+                        association loi 1901 à but non lucratif.
+                        L’association France Esports a pour objet de développer, promouvoir, encadrer la 
+                        pratique des sports électroniques dans un esprit d’équité et d’épanouissement humain, 
+                        s’inscrivant dans les valeurs et les principaux fondamentaux de l’Olympisme (Article 1 de ses statuts). 
+                        L’association souhaite représenter les intérêts communs du monde amateur, du monde professionnel ainsi que 
+                        des agents économiques du secteur des sports électroniques.
+                        <img class="mt-4" src="./assets/fes-pgw-2016-768x432.jpg" width="100%" /></p>
+                    <p></p>
                 </section>
                 <!--
                 ###
                 ### Right pannel
                 ###
                 -->
-                <section class="col-sm mt-5 d-flex flex-column">
+                <section class="">
                     <h4>Nos evenements à venir</h4>
                     <!-- Card that represent incoming events-->
-                    <div class="card text-black p-2">
+                    <div class="card text-black p-2 card-event">
                         <div class="card-body">
                             <h5 class="card-title">NextG Lan - 18/09/2022</h5>
-                            <p class="card-text">NextG - Lan #1, venez participez à notre Lan</p>
+                            <p class="card-text">NextG - Lan #1, venez participer à notre Lan</p>
                             <ul>
                                 <li>Plus de 3500€ de cash prize !</li>
-                                <li>Plus de 700€ de lot à remporter</li>
-                                <li>Des stand avec differente activité, VR, Simulation</li>
-                            </ul>
-                            <a href="event.php" class="btn btn-primary">Visualiser</a>
-                        </div>
-                    </div>
-                    <div class="card text-black p-2 my-2">
-                        <div class="card-body">
-                            <h5 class="card-title">NextG Lan - 18/09/2022</h5>
-                            <p class="card-text">NextG - Lan #1, venez participez à notre Lan</p>
-                            <ul>
-                                <li>Plus de 3500€ de cash prize !</li>
-                                <li>Plus de 700€ de lot à remporter</li>
-                                <li>Des stand avec differente activité, VR, Simulation</li>
+                                <li>Plus de 700€ de lots à remporter</li>
+                                <li>Des stands pour tester la réalité virtuelle et beaucoup d'autres activités</li>
                             </ul>
                             <a href="event.php" class="btn btn-primary">Visualiser</a>
                         </div>
@@ -158,6 +146,11 @@
             </section>
         </div>
     </div>
+    <!--
+    ###
+    ### Footer that contain copyright, newsletter and some utils links
+    ###
+    -->
     <footer class="bg-dark text-center text-lg-start text-white mt-5">
         <!-- Grid container -->
         <div class="container p-4 pb-0">
@@ -197,17 +190,12 @@
 
         <!-- Copyright -->
         <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-            © 2020 Copyright:
+            © 2022 Copyright:
             <a class="text-light" href="https://nextg-esport.com/">nextg-esport.com</a>
         </div>
         <!-- Copyright -->
     </footer>
 </body>
-<script src="https://code.jquery.com/jquery-3.3.1.min.js" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
-</script>
-<script src="js/navbar-behavior.js" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
 </script>
 
 </html>
--- js/navbar-behavior.js
+++ js/navbar-behavior.js
@@ -1,9 +1,22 @@
 $(function () {
+    changeNavbarStateOnScroll();
+
+    /**
+     * On scroll event
+     */
     $(window).on('scroll', function () {
+        changeNavbarStateOnScroll();
+    });
+
+    /**
+     * Add active class on navbar when scrool reach 10px
+     */
+    function changeNavbarStateOnScroll()
+    {
         if ( $(window).scrollTop() > 10 ) {
             $('.navbar').addClass('active');
         } else {
             $('.navbar').removeClass('active');
         }
-    });
+    }
 });
