a432719ecaf08b76f6f1b1f8d4aedccbc85b8e07
Pierre <pierre.gervais@socialmove.com>
2022-04-16 18:48:44 +0200

    Add html meta tag

--- index.php
+++ index.php
@@ -12,14 +12,21 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
+    <meta name="description" content="Suivez l'actualité de l'esport Français, accèder à nos événements passé et futur">
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="France - Esports présentation" />
+    <meta property="og:description" content="Suivez l'actualité de l'esport Français, accèder à nos événements passé et futur" />
+    <meta property="og:image" content="https://nextg-esport-vitrine.herokuapp.com/assets/logo-1024x1024.png" />
+    <meta property="og:url" content="PERMALINK" />
+    <meta property="og:site_name" content="France - Esports" />
+    <link rel="canonical" href="https://nextg-esport-vitrine.herokuapp.com/" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="styles/default.css">
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="js/navbar-behavior.js" crossorigin="anonymous"></script>
-    <title>NextG - Esport</title>
+    <title>France - Esports</title>
 </head>
 
 <body>
@@ -200,7 +207,7 @@
         <!-- Copyright -->
         <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
             © 2022 Copyright:
-            <a class="text-light" href="https://nextg-esport.com/">france-esport.com</a>
+            <a class="text-light" href="#">france-esport.com</a>
         </div>
         <!-- Copyright -->
     </footer>
