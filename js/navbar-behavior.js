b337628407cc599d80a81a0ee23ec9853777d651
Pierre <pierre.gervais@socialmove.com>
2022-04-18 08:22:22 +0200

    Add comment

--- js/navbar-behavior.js
+++ js/navbar-behavior.js
@@ -1,19 +1,19 @@
-$(function () {
+$(function() {
     changeNavbarStateOnScroll();
 
     /**
      * On scroll event
+     * Raise changeNavbarStateOnScroll()
      */
-    $(window).on('scroll', function () {
+    $(window).on('scroll', function() {
         changeNavbarStateOnScroll();
     });
 
     /**
      * Add active class on navbar when scrool reach 10px
      */
-    function changeNavbarStateOnScroll()
-    {
-        if ( $(window).scrollTop() > 10 ) {
+    function changeNavbarStateOnScroll() {
+        if ($(window).scrollTop() > 10) {
             $('.navbar').addClass('active');
         } else {
             $('.navbar').removeClass('active');
